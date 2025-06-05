ced73d7fa62d4021fdb283795d91283d562d1e61
Parsmedia-Alex <alex.meyer@parsmedia.info>
2025-06-05 18:26:23 +0200

    update

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -17,3 +17,9 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
+
+test('regression', async ({ page }) => {
+  await page.goto('https://playwright.dev');
+  await expect(page).toHaveScreenshot({fullPage: true});
+});
